3b782eef7734a943ce18692c1f43abd6f1236b58
Kendal-Bessette <kendalbessette>
2021-09-26 08:30:49 -0400

    Complete qualified assignment, ...

--- public/src/books.js
+++ public/src/books.js
@@ -11,10 +11,13 @@ function findBookById(books, id) {
 };
 
 function partitionBooksByBorrowedStatus(books) {
+ // Set up two arrays to partition the results  
   let availArray = [];
   let unavailArray = []; 
+ // Set up a new array to store the partitioned results  
   const bookStatus = []; 
     books.forEach(book => {
+// partition the results based on borrow status    
   const bookReturned = book.borrows[0].returned;
   if (bookReturned) {
     unavailArray.push(book);   
@@ -22,6 +25,7 @@ function partitionBooksByBorrowedStatus(books) {
     availArray.push(book);  
   } 
 });
+// return the array with partitioned results 
   bookStatus.push(availArray); 
   bookStatus.push(unavailArray); 
   return bookStatus; 
@@ -29,9 +33,11 @@ function partitionBooksByBorrowedStatus(books) {
 
 function getBorrowersForBook(book, accounts) {
   return book.borrows.map(borrow => {
+// find the accounts with an ID matching the borrow ID
   let account = accounts.find(acc => acc.id === borrow.id)
   return {...borrow, ...account}  
 })
+// limit results in array to 10
   .slice(0,10)
 };
 
